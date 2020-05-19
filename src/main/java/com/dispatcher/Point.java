a4e16ccdd84553ba09eafe0d3c78a2cafbf7065b
unknown <bartekwwb@gmail.com>
2020-05-19 17:39:36 +0200

    Added point distance test

--- src/main/java/com/dispatcher/Point.java
+++ src/main/java/com/dispatcher/Point.java
@@ -5,6 +5,11 @@ public class Point {
     private double x;
     private double y;
 
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
     Point(JSONObject jsonObject) {
         this.x = jsonObject.getJSONObject("pose").getJSONObject("position").getDouble("x");
         this.y = jsonObject.getJSONObject("pose").getJSONObject("position").getDouble("y");
--- src/test/java/com/dispatcher/PointTest.java
+++ src/test/java/com/dispatcher/PointTest.java
@@ -8,5 +8,9 @@ class PointTest {
 
     @Test
     void getDistance() {
+        Point point1 = new Point(5, 7);
+        Point point2 = new Point(3, 2);
+        double distance = point1.getDistance(point2);
+        assertEquals(5.385, distance, 0.001);
     }
 }
