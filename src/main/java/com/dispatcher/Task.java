5df33a342f960f52065db85e41e469204dcc1a5d
okalinowska <olgakalina@wp.pl>
2020-05-24 18:52:25 +0200

    ChooseRobotAndTask test fixed

--- src/main/java/com/dispatcher/Task.java
+++ src/main/java/com/dispatcher/Task.java
@@ -77,6 +77,10 @@ public class Task {
         return priority;
     }
 
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
     public Point getStart() {
         return start;
     }
